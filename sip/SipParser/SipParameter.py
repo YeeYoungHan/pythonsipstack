20dccaee6301d2c7ba046574223cf48a207aab61
Yee Young Han <websearch@naver.com>
2020-09-25 20:41:16 +0900

    클래스 변수를 멤버 변수로 수정함

--- sip/SipParser/SipParameter.py
+++ sip/SipParser/SipParameter.py
@@ -17,8 +17,10 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 '''
 
 class SipParameter():
-  strName = ''
-  strValue = ''
+
+  def __init__(self):
+    self.strName = ''
+    self.strValue = ''
 
   def Parse( self, strText, iStartPos ):
     self.Clear()
