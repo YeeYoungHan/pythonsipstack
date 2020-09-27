9ac436c0d192bd429949aeeea71d39662d588c23
Yee Young Han <websearch@naver.com>
2020-09-27 21:25:47 +0900

    SIP 용 MD5 문자열 생성 함수를 추가함

---
+++ test/TestSipPlatform/TestSipMd5.py
@@ -0,0 +1,24 @@
+''' 
+Copyright (C) 2020 Yee Young Han <websearch@naver.com> (http://blog.naver.com/websearch)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+'''
+
+from sip.SipPlatform.SipMd5 import SipMd5String
+
+strMd5 = SipMd5String( "1234" )
+
+if( strMd5 != "81dc9bdb52d04dc20036dbd8313ed055" ):
+  printf( "error" )
